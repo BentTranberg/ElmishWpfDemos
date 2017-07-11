f87fdeb4c3975551143e06ee67b6b5654bd740fb
BentTranberg <bent.tranberg@gmail.com>
2017-07-11 16:35:52 +0200

    Handling unhandled exceptions.

--- FirstDemo/Program.fs
+++ FirstDemo/Program.fs
@@ -9,6 +9,7 @@ module Types =
     type Msg =
         | Increment
         | Decrement
+        | CauseCrash
     
     type Model = { Count: int }
 
@@ -16,24 +17,52 @@ module State =
 
     open Types
 
+    let causeCrash() = 13 / 0 |> ignore
+
     let init() = { Count = 0 }
 
     let update (msg: Msg) (model: Model) =
         match msg with
         | Increment -> { model with Count = model.Count + 1 }
         | Decrement -> { model with Count = model.Count - 1 }
+        | CauseCrash ->
+            // causeCrash() // Uncomment this call if you want an exception here.
+            model
 
 module App =
 
+    open System.Windows
+    open System.Windows.Threading
     open Types
     open State
 
     let view _ _ =
         [ "Increment" |> Binding.cmd (fun _ m -> Increment)
           "Decrement" |> Binding.cmd (fun _ m -> Decrement)
+          "CauseCrash" |> Binding.cmd (fun _ m ->
+            // causeCrash() // Uncomment this call if you want an exception here.
+            CauseCrash)
           "Count" |> Binding.oneWay (fun m -> m.Count) ]
+
+    let elmishErrorHandler (messageToShow: string, ex: exn) =
+        Windows.MessageBox.Show(ex.Message) |> ignore
+        ()
+
+    let dispatcherUnhandledException (e: DispatcherUnhandledExceptionEventArgs) =
+        e.Handled <- true
+        Windows.MessageBox.Show(e.Exception.Message) |> ignore
+        ()
+
+    let windowLoaded _ =
+        let app = Windows.Application.Current
+        app.DispatcherUnhandledException.Add dispatcherUnhandledException
+        app.ShutdownMode <- ShutdownMode.OnMainWindowClose
+        ()
 
     [<EntryPoint;STAThread>]
     let main argv =
+        let window = FirstDemoViews.MainWindow()
+        window.Loaded.Add windowLoaded
         Program.mkSimple init update view
-        |> Program.runWindow (FirstDemoViews.MainWindow())
+        |> fun p -> { p with onError = elmishErrorHandler }
+        |> Program.runWindow window
