8a8b7f5e09c8fd5cd7ead4d550276cb7f1053435
BentTranberg <bent.tranberg@gmail.com>
2017-07-11 15:40:08 +0200

    Borrowed some source from Elmish.WPF.Counter(Views), so that we have something to play with.

--- FirstDemo/Program.fs
+++ FirstDemo/Program.fs
@@ -1,7 +1,39 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace FirstDemo
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+open Elmish
+open Elmish.WPF
+
+module Types =
+
+    type Msg =
+        | Increment
+        | Decrement
+    
+    type Model = { Count: int }
+
+module State =
+
+    open Types
+
+    let init() = { Count = 0 }
+
+    let update (msg: Msg) (model: Model) =
+        match msg with
+        | Increment -> { model with Count = model.Count + 1 }
+        | Decrement -> { model with Count = model.Count - 1 }
+
+module App =
+
+    open Types
+    open State
+
+    let view _ _ =
+        [ "Increment" |> Binding.cmd (fun _ m -> Increment)
+          "Decrement" |> Binding.cmd (fun _ m -> Decrement)
+          "Count" |> Binding.oneWay (fun m -> m.Count) ]
+
+    [<EntryPoint;STAThread>]
+    let main argv =
+        Program.mkSimple init update view
+        |> Program.runWindow (FirstDemoViews.MainWindow())
